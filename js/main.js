085c3ec9a98f51a33513f1c7219ab9b298372aca
Jordan <knightworks@pm.me>
2019-01-11 00:47:20 -0600

    scss converter

--- js/main.js
+++ js/main.js
@@ -4,11 +4,18 @@ new Vue({
         message: 'hello the'
     },
     created() {
+        this.getJson()
     },
     filters: {
     },
     computed: {
     },
     methods: {
+        getJson() {
+            console.log('hello')
+            let scssObj = "body {background: red; & > section {background: black;}}"
+            axois.get('math?scss=' + scssObj)
+            .then(res => console.log(res))
+        }
     },
 })
--- server.js
+++ server.js
@@ -1,15 +1,59 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const fs = require("fs")
 const path = require('path')
+const sass = require('node-sass')
+const { exec } = require('child_process');
 
 // Create Express App
 const app = express()
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('dist'))
+// FUNCTIONS
+const l = (val) => console.log(val)
+
+const lg = (val='nothing passed in') => {
+	l(val)
+	l(typeof val)
+	if (typeof val === 'object') {
+		Object.keys(val).forEach(i => lg(val[i]))
+	} else {
+		l(val.toString())
+	}
+	l('--------------------')
+}
 
 //
 app.get('/', function(req, res) {
-    console.log(path.join(__dirname, 'index.html'))
+    lg(path.join(__dirname, 'index.html'))
     res.sendFile(path.join(__dirname, 'index.html'));
 })
+
+app.get('/convert', function(req, res) {
+    let scss = req.query.scss
+    l(req.query)
+    l(typeof scss)
+    let css = sass.renderSync({ data: scss,
+      indentedSyntax: true,
+      outputStyle : 'compressed'
+    })
+    res.json({css : css.css.toString()})
+
+    
+})
+app.get('/math', function(req, res) {
+    inputs = req.query
+    let answer = 0
+    for (i in inputs) answer  += Number(inputs[i])
+    l('hello')
+    exec('npm run convert', (err, stdout, stderr) => {
+      l(`stdout: ${stdout}`);
+    });
+    res.json({inputs, answer})
+    
+})
+
+
 
 var server = app.listen(process.env.PORT || 3000, function() {console.log('********************** Server listening on port %s *', server.address().port);})
